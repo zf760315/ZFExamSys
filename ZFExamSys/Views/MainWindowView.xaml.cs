1383e203d09eb025f03a9375e7d2b486e544cd49
zf760315 <zufei760315@sina.com>
2023-07-24 22:41:37 +0800

    新建sqlite数据库

--- ZFExamSys/Views/MainWindowView.xaml.cs
+++ ZFExamSys/Views/MainWindowView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ZFExamSys
+namespace ZFExamSys.Views
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowView : Window
     {
-        public MainWindow()
+        public MainWindowView()
         {
             InitializeComponent();
         }
